764a25df4c209163cac25d3a5c19814a4445f137
Tolik <lukianenko.a.c@gmail.com>
2025-07-02 11:23:17 +0300

    fix: deployment bugs

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/uaitlab-test/",
+  // base: "/uaitlab-test/",
   plugins: [react()],
   resolve: {
     alias: {
